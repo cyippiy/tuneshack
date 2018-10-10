d74772fa016a8538bee30804943c1919da68b167
Chris Yip <chris.yip87@gmail.com>
2018-10-09 17:56:26 -0700

    Attempt user controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,17 +1,35 @@
 class Api::UsersController < ApplicationController
   def create
+    @user = User.new(user_params)
 
+    if @user.save
+      login(@user)
+      render "api/users/show"
+    else
+      render json: @user.errors.full_messages, status:422
+    end
   end
 
   def index
-
+    @user = User.all
   end
 
   def show
-
+    @user = User.find(params[:id])
   end
 
   def update
+    if @user == current_user
+      if @user.update(user_params)
 
+      else
+        flash.now[:errors] = @user.errors.full_messages
+    else
+      render :show
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:email,:password)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   validates :email, :password_digest, :session_token, presence: true
   validates :password length: {minimum: 6, allow_nil: true}
 
-  after_initialize ensure_session_token
+  after_initialize :ensure_session_token
 
   attr_reader :password
 

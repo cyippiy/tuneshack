6c671a768cd836608af35edb20d8b8edcf049211
Chris Yip <chris.yip87@gmail.com>
2018-10-10 14:47:39 -0700

    complete sessionReducer

--- frontend/tune_shack.jsx
+++ frontend/tune_shack.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
+import {signup, login, logout} from './util/session_api_util'
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
+  window.login = login;
+  window.signup = signup;
+  window.logout = logout;
   ReactDOM.render(<Root />, root);
 });

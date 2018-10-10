58d4bcf781b98453d1198efe4b8f9d0e0c370dec
Chris Yip <chris.yip87@gmail.com>
2018-10-10 16:45:55 -0700

    heroku config setup

--- frontend/tune_shack.jsx
+++ frontend/tune_shack.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {signup, login, logout} from './util/session_api_util'
+import {signup, login, logout} from './util/session_api_util';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
+  const store = configureStore();
   window.login = login;
   window.signup = signup;
   window.logout = logout;
-  ReactDOM.render(<Root />, root);
+  ReactDOM.render(<Root store={store} />, root);
 });

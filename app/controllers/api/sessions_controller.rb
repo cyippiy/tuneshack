3d9fa99a918c4513201d2c750f3a02167515a573
Chris Yip <chris.yip87@gmail.com>
2018-10-10 11:34:29 -0700

    output confirmation for successful logout

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -16,7 +16,7 @@ class Api::SessionsController < ApplicationController
     @user = current_user
     if @user
       logout
-      render 'api/users/show'
+      render json: ["User successfully logged off"], status: 200
     else
       render json: ["No user is currently signed on"], status: 404
     end

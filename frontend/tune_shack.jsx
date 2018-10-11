f3aea016a34683ad1f535fb7bf69437f94fa1974
Chris Yip <chris.yip87@gmail.com>
2018-10-10 18:10:26 -0700

    Root route complete

--- frontend/tune_shack.jsx
+++ frontend/tune_shack.jsx
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {signup, login, logout} from './util/session_api_util';
 import configureStore from './store/store';
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   const store = configureStore();
-  window.login = login;
-  window.signup = signup;
-  window.logout = logout;
+
+  // TESTING STARTS
+
+  // window.login = login;
+  // window.signup = signup;
+  // window.logout = logout;
+  // window.getState = store.getState;
+  // window.dispatch = store.dispatch;
+
+  // TESTING ENDS
   ReactDOM.render(<Root store={store} />, root);
 });

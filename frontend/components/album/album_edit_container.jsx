3d3aed5e9ad39bcd8979f1125bc60f963110b342
Chris Yip <chris.yip87@gmail.com>
2018-11-27 19:45:32 -0800

    album edit skeletons

---
+++ frontend/components/album/album_edit_container.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import AlbumDetails from './album'
+import values from 'lodash/values';
+import { fetchAlbum } from '../../actions/album_actions';
+import { fetchSongsAlbum } from '../../actions/song_actions';
+
+const mapStateToProps = (state, ownProps) => {
+    return ({
+        album: state.entities.albums[ownProps.match.params.id],
+        id: ownProps.match.params.id,
+        songs: state.entities.songs.songs
+    })
+}
+
+const mapDispatchToProps = dispatch => {
+    return ({
+        fetchAlbum: album => dispatch(fetchAlbum(album)),
+        fetchSongsAlbum: songs => dispatch(fetchSongsAlbum(songs))
+    })
+}
+export default connect(mapStateToProps, mapDispatchToProps)(AlbumEdit)

4c8c5303576918c46a1158d9e8bc3b453f992ca4
Chris Yip <chris.yip87@gmail.com>
2018-12-15 14:52:38 -0800

    icons

--- frontend/components/home.jsx
+++ frontend/components/home.jsx
@@ -16,8 +16,7 @@ import UserProfileContainer from './user/user_profile_container';
 
 class Home extends React.Component{
   render(){
-    return (
-      <div>
+    return <div>
         <header>
           <NavBarContainer />
         </header>
@@ -38,13 +37,13 @@ class Home extends React.Component{
               </Link>
               <Link className="box-right2" to="/albums/4">
                 <div className="caption">
-                <h1>MARS BRUNO holding it down</h1>
+                  <h1>MARS BRUNO holding it down</h1>
                 </div>
               </Link>
               <Link className="box-right3" to="/albums/5">
                 <div className="overlay">
                   <div className="caption">
-                  <h1>Elevven was just featured on ASOT</h1>
+                    <h1>Elevven was just featured on ASOT</h1>
                   </div>
                 </div>
               </Link>
@@ -53,19 +52,35 @@ class Home extends React.Component{
           <section className="splash-icons">
             <div className="splash-fans">
               <h1>Fans</h1>
+              <img src="https://bandcamp.com/img/homepage_2015/signups-fans-icon@3x.png" />
+              <h1>
+                Get instant streaming of your purchases, follow your
+                favorite artists, keep a wishlist, showcase your collection,
+                and explore the music of like-minded fans.
+              </h1>
             </div>
             <div className="splash-artists">
               <h1>Artists</h1>
+            <img src="https://bandcamp.com/img/homepage_2015/signups-artists-icon@3x.png" />
+              <h1>
+                Sell directly to your fans with total control over your
+                music and pricing. Easy access to your customers’ data,
+                real-time stats, music chart reporting, and more.
+              </h1>
             </div>
             <div className="splash-labels">
               <h1>Labels</h1>
+            <img src="https://bandcamp.com/img/homepage_2015/signups-labels-icon@3x.png" />
+              <h1>
+                Unified accounting and stats across all your artists, a
+                single fulfillment interface for all your merch, direct
+                payments on a per release basis, and a whole lot more.
+              </h1>
             </div>
           </section>
-          <section className="footer">
-          </section>
+          <section className="footer" />
         </section>
-      </div>
-    );
+      </div>;
   }
 }
 

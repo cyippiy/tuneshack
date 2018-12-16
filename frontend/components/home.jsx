228dc3bda09a0b3a1414543e1e482754d8265842
Chris Yip <chris.yip87@gmail.com>
2018-12-16 14:52:28 -0800

    footer github/linkedin links

--- frontend/components/home.jsx
+++ frontend/components/home.jsx
@@ -54,10 +54,11 @@ class Home extends React.Component{
               <h1>Fans</h1>
               <img src="https://bandcamp.com/img/homepage_2015/signups-fans-icon@3x.png" />
               <div className="description">
-              <p>Get instant streaming of your purchases, follow your
-                favorite artists, keep a wishlist, showcase your collection,
-                and explore the music of like-minded fans.
-              </p>
+                <p>
+                  Get instant streaming of your purchases, follow your
+                  favorite artists, keep a wishlist, showcase your
+                  collection, and explore the music of like-minded fans.
+                </p>
               </div>
               <div className="learn-button">
                 <button className="learn-more">Learn more</button>
@@ -65,48 +66,77 @@ class Home extends React.Component{
             </div>
             <div className="splash-artists">
               <h1>Artists</h1>
-            <img src="https://bandcamp.com/img/homepage_2015/signups-artists-icon@3x.png" />
-              <p>Sell directly to your fans with total control over your
+              <img src="https://bandcamp.com/img/homepage_2015/signups-artists-icon@3x.png" />
+              <p>
+                Sell directly to your fans with total control over your
                 music and pricing. Easy access to your customers’ data,
                 real-time stats, music chart reporting, and more.
               </p>
-            <div className="learn-button">
-              <button className="learn-more">Learn more</button>
-            </div>
+              <div className="learn-button">
+                <button className="learn-more">Learn more</button>
+              </div>
             </div>
             <div className="splash-labels">
               <h1>Labels</h1>
-            <img src="https://bandcamp.com/img/homepage_2015/signups-labels-icon@3x.png" />
-              <p>Unified accounting and stats across all your artists, a
+              <img src="https://bandcamp.com/img/homepage_2015/signups-labels-icon@3x.png" />
+              <p>
+                Unified accounting and stats across all your artists, a
                 single fulfillment interface for all your merch, direct
-                payments on a per release basis, and a whole lot more.</p>
-            <div className="learn-button">
-              <button className="learn-more">Learn more</button>
-            </div>
+                payments on a per release basis, and a whole lot more.
+              </p>
+              <div className="learn-button">
+                <button className="learn-more">Learn more</button>
+              </div>
             </div>
           </section>
           <section className="footer">
             <section className="foot-col">
-              <Link className="fake" to="#">About Us</Link>
-              <Link className="fake" to="#">Fair Trade Music</Link>
-              <Link className="fake" to="#">Jobs</Link>
-              <Link className="fake" to="#">Terms of Use</Link>
-              <Link className="fake" to="#">Privacy</Link>
-              <Link className="fake" to="#">Copyright Policy</Link>
+              <Link className="fake" to="#">
+                About Us
+              </Link>
+              <Link className="fake" to="#">
+                Fair Trade Music
+              </Link>
+              <Link className="fake" to="#">
+                Jobs
+              </Link>
+              <Link className="fake" to="#">
+                Terms of Use
+              </Link>
+              <Link className="fake" to="#">
+                Privacy
+              </Link>
+              <Link className="fake" to="#">
+                Copyright Policy
+              </Link>
             </section>
             <section className="foot-col">
-              <Link className="fake" to="#">Gift Cards</Link>
-              <Link className="fake" to="#">TuneShack Daily</Link>
-              <Link className="fake" to="#">Facebook</Link>
-              <Link className="fake" to="#">Contact/Help</Link>
+              <Link className="fake" to="#">
+                Gift Cards
+              </Link>
+              <Link className="fake" to="#">
+                TuneShack Daily
+              </Link>
+              <Link className="fake" to="#">
+                Facebook
+              </Link>
+              <Link className="fake" to="#">
+                Contact/Help
+              </Link>
             </section>
+            <section className="foot-col" />
             <section className="foot-col">
-            </section>
-            <section className="foot-col">
-              <p>Created By: Chris Yip</p>
-            <Link className="contact-links" to="#">LinkedIn</Link>
-            <Link className="contact-links" to="#">GitHub</Link>
-              GitHub
+              <h2>Created By: Chris Yip</h2>
+            <i className="fas fa-igloo"></i>
+
+                <i className="fab fa-linkedin-in" />
+            <a className="contact-links" href="https://www.linkedin.com/in/cyip87/">
+                LinkedIn
+              </a>
+
+            <a className="contact-links" href="https://github.com/cyippiy">
+                GitHub
+              </a>
             </section>
           </section>
         </section>

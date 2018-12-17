4bb15d00c623ddeb6f7871043b47ee56da820c5c
Chris Yip <chris.yip87@gmail.com>
2018-12-17 14:05:37 -0800

    separate footer auth into own component

---
+++ frontend/components/footer_bar/footer_auth.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const footer_auth = () => {
+
+    return (
+    <section className="footer-auth">
+        <section className="footer-left">
+            <h3>TuneShack</h3>
+        </section>
+        <section className="footer-right">
+            <h3>Chris Yip</h3>
+            <a href="https://www.linkedin.com/in/cyip87/">LinkedIn</a>
+            <a href="https://github.com/cyippiy">GitHub</a>
+        </section>
+    </section>);
+}
+
+export default footer_auth;

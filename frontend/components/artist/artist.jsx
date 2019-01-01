f8c716a8451c2d95b417cbfecd22fe6dc57d8382
Chris Yip <chris.yip87@gmail.com>
2018-12-31 23:46:52 -0800

    artist component links and references artist info correctly

--- frontend/components/artist/artist.jsx
+++ frontend/components/artist/artist.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 
 class Artist extends React.Component{
     constructor(props){
         super(props);
-        this.state = {
-            artist: 0;
-        }
     }
     componentDidMount(){
-        this.props.fetchUser(this.state.artist);
+        this.props.fetchUser(this.props.id);
+    }
+
+    render(){
+        if (!this.props.id){
+            return null;
+        }
+        else if (!this.props.artist){
+            return null;
+        }
+        else{
+            let display = <Link to={`/artists/${this.props.id}`}>
+                {this.props.artist.band_name}
+              </Link>;
+            return display;
+        }
     }
 }
 
+export default Artist;

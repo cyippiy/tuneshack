b08d8273615dd183b29ac6b6ce4ec1ff409525ed
Chris Yip <chris.yip87@gmail.com>
2018-12-31 21:32:31 -0800

    Artist component

--- frontend/components/artist/artist.jsx
+++ frontend/components/artist/artist.jsx
@@ -1,1 +1,15 @@
 import React from 'react';
+
+
+class Artist extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            artist: 0;
+        }
+    }
+    componentDidMount(){
+        this.props.fetchUser(this.state.artist);
+    }
+}
+

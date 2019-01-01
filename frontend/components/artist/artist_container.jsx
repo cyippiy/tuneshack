6a232a6e830e362cafae8004d0274a9167e7a9da
Chris Yip <chris.yip87@gmail.com>
2018-12-31 23:46:42 -0800

    artist component links and references artist info correctly

--- frontend/components/artist/artist_container.jsx
+++ frontend/components/artist/artist_container.jsx
@@ -1,11 +1,18 @@
 import { connect } from 'react-redux';
 import Artist from './artist';
-import { fetchUser } from "../../actions/session_actions";
+import { fetchUser } from '../../actions/session_actions';
+
+const mapStateToProps = (state,ownProps) => {
+    return {
+        id: ownProps.id,
+        artist: state.entities.users[ownProps.id]
+    }
+}
 
 const mapDispatchToProps = dispatch => {
     return ({
-        fetchUser: user => dispatch(fetchUser(user));
+        fetchUser: user => dispatch(fetchUser(user))
     })
 }
 
-export default connect(_,mapDispatchToProps)(Artist);
+export default connect(mapStateToProps,mapDispatchToProps)(Artist);

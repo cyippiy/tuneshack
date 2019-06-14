6c0791e3cd642a10bf44b12d6d3e2cd034b9d92d
Chris Yip <chris.yip87@gmail.com>
2019-06-13 17:24:53 -0700

    add receiveUsers

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -4,6 +4,7 @@ export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
 export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER";
 export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
 export const RECEIVE_USER = "RECEIVE_USER";
+export const RECEIVE_USERS = "RECEIVE_USERS";
 
 const receiveCurrentUser = user => {
   return {
@@ -28,7 +29,14 @@ const receiveUser = user => {
     type: RECEIVE_USER,
     user
   }
-}
+};
+
+const receiveUsers = users => {
+  return({
+    type: RECEIVE_USERS,
+    users
+  })
+};
 
 export const login = user => dispatch =>{
   return APIUtil.login(user).then(user => dispatch(receiveCurrentUser(user)),

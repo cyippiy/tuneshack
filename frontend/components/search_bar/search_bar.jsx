e3dc37a36229c57589210c1b4b7daa14e0de3647
Chris Yip <chris.yip87@gmail.com>
2019-06-13 17:27:37 -0700

    add search(). currently working on timeout delay for search

--- frontend/components/search_bar/search_bar.jsx
+++ frontend/components/search_bar/search_bar.jsx
@@ -5,13 +5,31 @@ class SearchBar extends React.Component{
         super(props);
         this.state = {
             query: "",
-            results: []
+            results: [],
+            typingTimeout: 0
+        }
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.search = this.search.bind(this);
+    }
+
+    search() {
+        let results = [];
+        if (this.state.query){
+            if (this.state.query !== ""){
+                searchUsers(this.state.query).then(res => results=[...res]);
+            }
         }
     }
 
     handleInputChange() {
+        if (this.state.typingTimeout){
+            clearTimeout(this.state.typingTimeout);
+        }
         this.setState({
-            query: this.search.value
+            query: this.search.value,
+            typingTimeout: setTimeout(() => {
+                this.search()
+            })
         })
     }
 

3b06ca30f71b368fc6abc8e59efcb5f93c2086b1
Chris Yip <chris.yip87@gmail.com>
2019-06-13 17:26:41 -0700

    dispatchToProps now includes fetch searcher users, albums, and songs

--- frontend/components/search_bar/search_bar_container.jsx
+++ frontend/components/search_bar/search_bar_container.jsx
@@ -1,12 +1,18 @@
 import {connect} from 'react-redux';
 import SearchBar from 'search_bar';
+import { fetchSearchUsers } from '../../actions/session_actions';
+import { fetchSearchAlbums } from '../../actions/album_actions';
+import { fetchSearchSongs } from '../../actions/song_actions';
 
 const mapStateToProps = state => {(
 
 )};
 
 const mapDispatchToProps = dispatch => ({
-    
+    searchUsers: search => dispatch(fetchSearchUsers(search)),
+    searchAlbums: search => dispatch(fetchSearchAlbums(search)),
+    searchSongs: search => dispatch(fetchSearchSongs(search)),
+
 });
 
 export default connect(mapStateToProps,mapDispatchToProps)(SearchBar);
